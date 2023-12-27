5a061b8d218b1bd4ac817f068c3cfb3661399b73
ZigaoWang <zigao.wang24@gmail.com>
2023-12-27 09:33:02 +0800

    Upload 1019-2

--- 1109-开关灯/解法2/main.cpp
+++ 1109-开关灯/解法2/main.cpp
@@ -1,10 +1,29 @@
+// 1109：开关灯
+// 解法2
 #include <iostream>
 
 using namespace std;
 
 const int N = 5010;
-int 
+int num[N];
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 2; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j % i == 0) {
+                if (num[j] == 1) num[j] = 0;
+                else num[j] = 1;
+            }
+        }
+    }
+
+    cout << 1;
+    for (int i = 2; i <= n; i++) {
+        if (num[i] == 0) {
+            cout << "," << i;
+        }
+    }
     return 0;
 }
