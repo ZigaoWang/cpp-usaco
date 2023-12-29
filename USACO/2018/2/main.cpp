158920566036bb6716c0aea2faeb6cf2d65d8518
ZigaoWang <zigao.wang24@gmail.com>
2023-12-29 22:20:51 +0800

    Update main.cpp

--- USACO/2018/2/main.cpp
+++ USACO/2018/2/main.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
-
 int main() {
     ifstream fin("blist.in");
     ofstream fout("blist.out");
     int N;
     fin >> N;
 
-    // Assuming the maximum time can be 1000 based on problem constraints
-    int bucketChanges[1001] = {};  // Initialize changes in bucket usage to 0
+    int bucketChanges[1010] = {0};
 
     int si, ti, bi;
     for (int i = 0; i < N; i++) {
         fin >> si >> ti >> bi;
-        bucketChanges[si] += bi; // Increase buckets at start time
-        bucketChanges[ti] -= bi; // Decrease buckets at end time
+        bucketChanges[si] += bi;
+        bucketChanges[ti] -= bi;
     }
 
-    // Determine the maximum number of buckets required at any time
     int maxBuckets = 0;
     int currentBuckets = 0;
     for (int i = 0; i < 1001; i++) {
-        currentBuckets += bucketChanges[i]; // Update current bucket usage
+        currentBuckets += bucketChanges[i];
         if (currentBuckets > maxBuckets) {
             maxBuckets = currentBuckets;
         }
