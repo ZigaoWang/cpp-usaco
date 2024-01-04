ff7a8ce71aa1d0d40aefa89b327ec0f1277d8401
ZigaoWang <zigao.wang24@gmail.com>
2024-01-04 21:56:09 +0800

    Update

--- USACO/2021/2/main.cpp
+++ USACO/2021/2/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int a[10];
+
+int main(){
+    for(int i=1;i<=7;++i)
+        cin >> a[i];
+    sort(a+1,a+7+1);
+    cout << a[1] << " ";
+    cout << a[2] << " ";
+    cout << a[7]-a[1]-a[2] << endl;
     return 0;
 }
