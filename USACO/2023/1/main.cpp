18f989f94dc65902e81fe980ab82f1ebf7ab6e2d
ZigaoWang <zigao.wang24@gmail.com>
2024-01-05 21:41:02 +0800

    Update USACO

--- USACO/2023/1/main.cpp
+++ USACO/2023/1/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+
 using namespace std;
+
+typedef long long ll;
+
+const int MAXN = 2e5+5;
+ll heights[MAXN];
+int n, m;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> heights[i];
+    for (int i = 0; i < m; i++) {
+        int x; cin >> x;
+        int l = 0;
+        for (int j = 0; j < n && l < x; j++) {
+            int diff = max(0, (int)min(heights[j], (ll)x)-l);
+            heights[j] += diff;
+            l += diff;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << heights[i] << '\n';
+    }
+    
 }
