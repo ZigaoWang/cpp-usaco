379d6445b33fcf7aa7df454879ed62cf91f8004d
ZigaoWang <zigao.wang24@gmail.com>
2024-01-06 20:11:12 +0800

    Update USACO

--- USACO/2023/1/main.cpp
+++ USACO/2023/1/main.cpp
@@ -10,9 +10,12 @@ int n, m;
 
 int main() {
     cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> heights[i];
+    for (int i = 0; i < n; i++) {
+        cin >> heights[i];
+    }
     for (int i = 0; i < m; i++) {
-        int x; cin >> x;
+        int x;
+        cin >> x;
         int l = 0;
         for (int j = 0; j < n && l < x; j++) {
             int diff = max(0, (int)min(heights[j], (ll)x)-l);
