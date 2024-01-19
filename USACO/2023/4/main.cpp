361c2ddaf8257f9301e9209f47c075caf3da98d2
ZigaoWang <zigao.wang24@gmail.com>
2024-01-19 22:32:54 +0800

    Update USACO

--- USACO/2023/4/main.cpp
+++ USACO/2023/4/main.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main() {
     int n = 0;
     cin >> n;
-    int infect[] = {0};
+    char infect[n];
     for (int i = 0; i < n; i++) {
         cin >> infect[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (infect[i] == '1') {
+            cout << i + 1 << endl;
+        }
     }
     return 0;
 }
