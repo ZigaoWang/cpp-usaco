aa6a53fb735b2c830b2eaa69937901de22f1d8fd
ZigaoWang <zigao.wang24@gmail.com>
2024-01-19 22:25:20 +0800

    Update USACO

---
+++ USACO/2023/4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+const int MAXN = 3 * 10e5;
+int main() {
+    int n = 0;
+    cin >> n;
+    int infect[MAXN] = {0};
+    for (int i = 0; i < n; i++) {
+        cin >> infect[i];
+    }
+    return 0;
+}
