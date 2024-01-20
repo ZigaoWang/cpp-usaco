58df009246867b1a36592753e7799009f55bafd0
ZigaoWang <zigao.wang24@gmail.com>
2024-01-20 19:39:15 +0800

    Update USACO

--- USACO/2023/4/main.cpp
+++ USACO/2023/4/main.cpp
@@ -8,12 +8,20 @@ int main() {
         cin >> infect[i];
     }
     int count[] = {0};
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n - i; j++) {
+//            if (infect[i + j] == '1') {
+//                count[i + j] = j;
+//            }
+//            else {
+//                count[i] = 0;
+//            }
+//        }
+//    }
     for (int i = 0; i < n; i++) {
-        count[i] = 0;
-        for (int j = 0; j < n - i - 1; j++) {
-
+        for (int j = 0; j < count[i]; j++) {
             if (infect[i + j] == '1') {
-                count[i] = j;
+                count[i + j] = j;
             }
         }
     }
