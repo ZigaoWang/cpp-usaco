a6d8af8e0ce03eb75ef24a209c800429512956f6
ZigaoWang <zigao.wang24@gmail.com>
2024-01-28 14:56:46 +0800

    Update USACO

--- USACO/2024/1/main.cpp
+++ USACO/2024/1/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int type[1000];
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int test = 0;
+    cin >> test;
+    int num = 0;
+    for (int i = 0; i < test; i++) {
+        cin >> num;
+        int temp[1000] = {0};
+        for (int j = 1; j <= num; j++) {
+            cin >> type[j];
+            if (j > 1 && type[j] == type[j - 1]) {
+                temp[type[j]] = 1;
+            }
+            if (j > 2 && type[j] == type[j - 2]) {
+                temp[type[j]] = 1;
+            }
+        }
+        int possible = false;
+        int place = 0;
+        for (int k = 1; k <= num; k++) {
+            if (temp[k] == 1) {
+                place++;
+                if (place != 1) {
+                    cout << " ";
+                }
+                cout << k;
+                possible = true;
+            }
+        }
+        if (possible == false) {
+            cout << "-1";
+        }
+        cout << endl;
+//        sort(temp, temp + num);
+//        for (int j = 0; j < num; j++) {
+//            if (temp[j] != 0) {
+//                cout << temp[j];
+//                if (j < num - 1) {
+//                    cout << " ";
+//                }
+//            }
+//            if (temp[0] == 0 && temp[num - 1] == 0) {
+//                cout << "-1";
+//            }
+//            while (temp[j] == temp[j + 1]) {
+//                j++;
+//            }
+//        }
+//        cout << endl;
+    }
+
+
     return 0;
 }
